0dee25586fb6cfa94a107ac60d31eab9306e4334
Tor4ubas <alex1029@yandex.ru>
2024-05-10 18:12:19 +0300

    Добавлены шаблоны

--- promo/urls.py
+++ promo/urls.py
@@ -7,7 +7,7 @@ app_name = PromoConfig.name
 
 urlpatterns = [
     path('', PromoListView.as_view(), name='promos'),
-
+    path('list/', PromoListView.as_view(), name='promo_list'),
     path('create/', PromoCreateView.as_view(), name='promo_create'),
     path('update/<int:pk>/', PromoUpdateView.as_view(), name='promo_update'),
     path('delete/<int:pk>/', PromoDeleteView.as_view(), name='promo_delete'),
