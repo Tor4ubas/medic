a4ea616c33c52526d65380d08cf7a5dfd3c536b5
Tor4ubas <alex1029@yandex.ru>
2024-05-10 15:50:07 +0300

    Добавлена новая модель

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'doctors', 'user_app', 'main',
+    'doctors', 'user_app', 'main', 'promo',
 ]
 
 MIDDLEWARE = [
---
+++ promo/models.py
@@ -0,0 +1,19 @@
+
+from django.conf import settings
+from django.db import models
+
+
+class Promo(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Наименование')
+    sub_title = models.CharField(max_length=200, null=True, verbose_name='Предложение')
+    description = models.TextField(null=True, verbose_name='Описание')
+    image = models.ImageField(upload_to='promo/', null=True, verbose_name='Изображение',)
+    is_active = models.BooleanField(default=False, verbose_name='Активно')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Акция'
+        verbose_name_plural = 'Акции'
+        permissions = [('set_promo_active_status', 'Can activate/deactivate promo')]
